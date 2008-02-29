d254687956bd8060058f6496d3129a45f1cf10fc
andreas <andreas@immerda.ch>
2008-02-29 18:31:49 +0000

    + imapproxy module * verbesserung phpmyadmin, etc

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,37 @@
-# modules/skeleton/manifests/init.pp - manage skeleton stuff
+# modules/imapproxy/manifests/init.pp - manage imapproxy stuff
 # Copyright (C) 2007 admin@immerda.ch
 #
 
-# modules_dir { "skeleton": }
+# modules_dir { "imapproxy": }
 
-class skeleton {
+class imapproxy {
+    $modulename = "imapproxy"
+    $pkgname = "up-imapproxy"
+    $gentoocat = "net-mail"
+    $cnfname = "imapproxy.conf"
+    $cnfpath = "/etc"
+
+
+    package { $pkgname:
+        ensure => present,
+        category => $operatingsystem ? {
+            gentoo => $gentoocat,
+            default => '',
+        }
+    }
+
+    file{
+        "${cnfpath}/${cnfname}":
+            source => [
+                "puppet://$server/dist/${modulename}/${fqdn}/${cnfname}",
+                "puppet://$server/${modulename}/${fqdn}/${cnfname}",
+                "puppet://$server/${modulename}/${cnfname}"
+            ],
+            owner => root,
+            group => 0,
+            mode => 0444,
+            require => Package[$pkgname],
+    }
+
 
 }
