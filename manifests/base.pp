52ce456fac420912124b25cf41c3cff5d32c3a7e
mh <mh@immerda.ch>
2014-11-17 22:17:03 +0100

    fix the init script so the pidfile is being used

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,20 +1,20 @@
+# basic configuration
 class imapproxy::base {
 
-  package { up-imapproxy:
+  package {'up-imapproxy':
     ensure => present,
-  }
-
-  file{'/etc/imapproxy.conf':
+  } -> exec{'fix_imapproxy_init_script':
+    command => ' sed -i \'s@killproc @killproc -p /var/run/imapproxy.pid @\' /etc/init.d/imapproxy',
+    onlyif  => 'grep -q \'killproc /\' /etc/init.d/imapproxy',
+  } -> file{'/etc/imapproxy.conf':
     source => [ "puppet:///modules/site_imapproxy/${::fqdn}/imapproxy.conf",
-                "puppet:///modules/site_imapproxy/imapproxy.conf",
-                "puppet:///modules/imapproxy/imapproxy.conf" ],
-    require => Package[up-imapproxy],
-    notify => Service[imapproxy],
-    owner => root, group => 0, mode => 0644;
-  }
-
-  service {'imapproxy':
-    ensure  => running,
+                'puppet:///modules/site_imapproxy/imapproxy.conf',
+                'puppet:///modules/imapproxy/imapproxy.conf' ],
+    owner => root,
+    group => 0,
+    mode  => '0644';
+  } ~> service {'imapproxy':
+    ensure => running,
     enable => true,
   }
 }
