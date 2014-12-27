f63438a5fe566789cbb85c0a7f75c198de3e9b1e
mh <mh@immerda.ch>
2014-12-27 10:58:47 +0100

    Revert "fix the init script so the pidfile is being used"
    This reverts commit 52ce456fac420912124b25cf41c3cff5d32c3a7e.
    
    It is fixed https://bugzilla.redhat.com/show_bug.cgi?id=1164925
    
    Conflicts:
    	manifests/base.pp

--- manifests/base.pp
+++ manifests/base.pp
@@ -3,16 +3,13 @@ class imapproxy::base {
 
   package {'up-imapproxy':
     ensure => present,
-  } -> exec{'fix_imapproxy_init_script':
-    command => 'sed -i \'s@killproc @killproc -p /var/run/imapproxy.pid @\' /etc/init.d/imapproxy',
-    onlyif  => 'grep -q \'killproc /\' /etc/init.d/imapproxy',
   } -> file{'/etc/imapproxy.conf':
     source => [ "puppet:///modules/site_imapproxy/${::fqdn}/imapproxy.conf",
                 'puppet:///modules/site_imapproxy/imapproxy.conf',
                 'puppet:///modules/imapproxy/imapproxy.conf' ],
-    owner => root,
-    group => 0,
-    mode  => '0644';
+    owner  => root,
+    group  => 0,
+    mode   => '0644';
   } ~> service {'imapproxy':
     ensure => running,
     enable => true,
