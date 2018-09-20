abfcf0d83b9056ab323168e6bab39a60e1b151e3
mh <mh@immerda.ch>
2018-09-20 09:43:29 +0200

    use lookup instead hiera_hash + linting

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,7 +4,7 @@
 #
 # install and manage imapproxy
 class imapproxy (
-  $merged_options   = hiera_hash('imapproxy::options',{}),
+  $merged_options   = lookup('imapproxy::options', Hash, 'hash', {}),
   $manage_shorewall = false,
   $manage_monit     = false,
 ) {
@@ -19,15 +19,15 @@ class imapproxy (
   }
   $real_options = merge($default_options,$merged_options)
 
-  case $::operatingsystem {
-    gentoo: { include imapproxy::gentoo }
-    default: { include imapproxy::base }
+  case $facts['operatingsystem'] {
+    'Gentoo': { include ::imapproxy::gentoo }
+    default: { include ::imapproxy::base }
   }
 
   if $manage_shorewall {
-    include shorewall::rules::out::imap
+    include ::shorewall::rules::out::imap
   }
   if $manage_monit {
-    include imapproxy::monit
+    include ::imapproxy::monit
   }
 }
